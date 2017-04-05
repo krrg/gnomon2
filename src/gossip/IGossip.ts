9923f81210b7a1e4353b7eced96de40911be6569
Ken R <krrgithub@gmail.com>
2017-04-05 00:44:14 -0600

    WIP!

--- src/gossip/IGossip.ts
+++ src/gossip/IGossip.ts
@@ -9,6 +9,8 @@ export interface IGossip {
     sendMessage(message: IMessage): any;
     filterMessages(senderId: string): Promise<Array<string>>;
 
+    subscribeToSender(senderId: string, callback: (msg: IMessage) => any): void;
+
 }
 
 export default IGossip;
