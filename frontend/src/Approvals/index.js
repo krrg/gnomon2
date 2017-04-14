6adaeb2e35cc666956c74d0e7dc355e2d67eb631
Ken R <krrgithub@gmail.com>
2017-04-13 22:03:22 -0600

    WIP

--- frontend/src/Approvals/index.js
+++ frontend/src/Approvals/index.js
@@ -1,12 +1,79 @@
 import React from "react";
+import axios from "axios";
 
 export default class Approvals extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            timestamps: {}
+        }
+    }
+
+    componentDidMount() {
+        this.getSubscriptions().forEach(this.reloadTimestampsFor)
+    }
+
+    reloadTimestampsFor = (workerId) => {
+        const timestampsPromise = axios.get(`/api/timestamps/${workerId}`)
+
+        timestampsPromise.then((resp) => {
+            const currentState = this.state;
+            currentState.timestamps[workerId] = resp.data;
+            this.setState(currentState);
+        })
+
+        timestampsPromise.catch((resp) => {
+            const currentState = this.state;
+            currentState.timestamps[workerId] = [];
+            this.setState(currentState);
+        })
+    }
+
+    getSubscriptions = () => {
+        return this.props.userSettings.isLoggedIn ? this.props.userSettings.subscriptions : [];
+    }
+
+    handleNewSubscription = (e) => {
+        e.preventDefault();
+        if (! this.refs.workerId.value) {
+            return;
+        }
+
+        axios.post('/api/userSubscriptions', {
+            subscriptionId: this.refs.workerId.value
+        }).then((resp) => {
+            this.props.userSettings.requestReload();
+        });
+    }
+
+    renderSubscriptions = (subscriptionIds) => {
+        return subscriptionIds.map((sid) => {
+            return (
+                <div key={sid}>
+                    <h3>Subscription ID: {sid}</h3>
+                    {JSON.stringify(this.state.timestamps[sid])}
+                </div>
+            )
+        })
+    }
+
     render() {
+        console.log(this.props.userSettings);
+
         return (
-            <div>
+            <div className="gn-container">
                 <h1>Timesheet Approvals</h1>
-                <p></p>
+                <p>You are currently subscribed to {this.getSubscriptions().length} workers.</p>
+                <p>Your signing ID is: <b>{this.props.userSettings.signing_id}</b></p>
+                <form>
+                    <input ref='workerId' placeholder='Sender id' />
+                    <button onClick={this.handleNewSubscription}>Subscribe to Worker</button>
+                </form>
+                <hr />
+
+                {this.renderSubscriptions(this.getSubscriptions())}
             </div>
         )
     }
--- frontend/src/Emails/index.js
+++ frontend/src/Emails/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import axios from "axios";
 
 export default class Emails extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            subscriptions: []
+        }
+
+        axios.get(`/api/subscriptions`)
+            .then((sub) => {
+                this.state.subscriptions = sub;
+            })
+    }
+
     render() {
         return (
-            <div>
+            <div className="gn-container">
                 <h1>Email Notifications</h1>
+                <p>This node is configured to send emails for: </p>
+                {JSON.stringify(this.state.subscriptions)}
             </div>
         )
     }
