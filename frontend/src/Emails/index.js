d25e4bc2602b60270da110b56b218523ff3e493e
Ken Reese <krrgithub@gmail.com>
2017-04-17 14:47:36 -0600

    Good nuf

--- frontend/src/Emails/index.js
+++ frontend/src/Emails/index.js
@@ -10,19 +10,46 @@ export default class Emails extends React.Component {
             subscriptions: []
         }
 
+    }
 
+    componentDidMount() {
+        axios.get(`/api/subscriptions`)
+            .then((resp) => {
+                this.setState({
+                    subscriptions: resp.data
+                })
+            })
+    }
+
+    handleNewSubscription = () => {
+        axios.post(`/api/subscriptions`, {
+            'senderId': this.refs.senderId.value,
+            'workerId': this.refs.workerId.value
+        })
     }
 
     renderSubscriptionForm = () => {
         return (
             <div>
-                
+                <form>
+                    <input type='text' ref='senderId' placeholder='senderId' />
+                    <input type='text' ref='workerId' placeholder='workerId' />
+                    <button onClick={this.handleNewSubscription}>Send me email updates!</button>
+                </form>
             </div>
         )
     }
 
     renderSubscriptionList = () => {
-
+        return this.state.subscriptions.map((subscription) => {
+            return (
+                <div>
+                    Worker ID: {subscription.workerId}
+                    &nbsp;
+                    Sender ID: {subscription.senderId}
+                </div>
+            )
+        })
     }
 
     render() {
@@ -30,7 +57,10 @@ export default class Emails extends React.Component {
             <div className="gn-container">
                 <h1>Email Notifications</h1>
                 <p>This node is configured to send emails for: </p>
-                {JSON.stringify(this.state.subscriptions)}
+                {this.renderSubscriptionForm()}
+                <hr />
+                {this.renderSubscriptionList()}
+
             </div>
         )
     }
