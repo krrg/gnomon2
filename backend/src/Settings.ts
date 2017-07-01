765c5013f34c4750a78df6ad6dedbd7d4bbf94fc
Ken Reese <krrg@users.noreply.github.com>
2017-06-30 20:06:56 -0600

    Update Settings.ts

--- backend/src/Settings.ts
+++ backend/src/Settings.ts
@@ -6,13 +6,15 @@ const Settings = {
         host: '127.0.0.1'
     },
     Login: {
-        client_id: '119242185034-3o7n83u0gvu16qejgprbbbftj07ua1qr.apps.googleusercontent.com',
-        client_secret: 'r0WCcPt27vW3oIR-TVSOB1eV',
+        
+        /* Note:  These are not actually working anymore */
+        client_id: '<Google Client ID>',
+        client_secret: '<Google Client Secret>',
         auth_return_url: 'http://localhost:4000/api/login/return'
     },
     Email: {
-        username: 'gnomondev@gmail.com',
-        password: 'bzvGuZ9!eLLt%omxFvUKmxYjLZp$$4Bz'
+        username: '<dev>@<example.com>',
+        password: '<password>'
     }
 
 
